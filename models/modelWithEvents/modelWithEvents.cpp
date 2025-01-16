2e0df19bafd415574c7bc8bbdd80cbb7bce20c05
rendayigit <renda.yigit@gmail.com>
2025-01-16 11:48:26 +0300

    update model events

--- models/modelWithEvents/modelWithEvents.cpp
+++ models/modelWithEvents/modelWithEvents.cpp
@@ -1,57 +1,42 @@
 #include "modelWithEvents.hpp"
-#include "common/modelEvent.hpp"
+
 #include <iostream>
 
 #include "sim_services/Executive/include/exec_proto.h"
 
-void ModelWithEvents::createOneShotEvent() {
-  // Create a model event
-  auto *event = new ModelEvent;
+ModelWithEvents::ModelWithEvents()
+    : m_oneShotEvent(new ModelEvent), m_scheduledEvent(new ModelEvent) {}
 
+void ModelWithEvents::createOneShotEvent() {
   // Set the event callback function
-  event->setEventFunction([this]() {
+  m_oneShotEvent->setEventFunction([this]() {
     std::cout << "One shot event setEventFunction Entered \t@ " << exec_get_sim_time() << std::endl;
   });
 
-  // Set the event name
-  event->set_name("My Sample Model One Shot Event");
-
   // Set the event cycle
-  event->set_cycle(0);
+  m_oneShotEvent->setCycleTime(0);
 
   // Set the event trigger time. The event will be triggered at this time.
-  event->setTriggerTime(2.2);
+  m_oneShotEvent->setTriggerTime(2.2);
 
   // Activate the event
-  event->activate();
-
-  // Add the event to the event manager
-  event_manager_add_event(event);
+  m_oneShotEvent->activate();
 }
 
 void ModelWithEvents::createScheduledEvent() {
-  // Create a model event
-  auto *event = new ModelEvent;
-
   // Set the event callback function
-  event->setEventFunction([this]() {
+  m_scheduledEvent->setEventFunction([this]() {
     std::cout << "Scheduled event setEventFunction Entered \t@ " << exec_get_sim_time()
               << std::endl;
   });
 
-  // Set the event name
-  event->set_name("My Sample Model Scheduled Event");
-
   // Set the event cycle
-  event->set_cycle(1);
+  m_scheduledEvent->setCycleTime(1);
 
   // Set the event trigger time. The event will be triggered at this time. This will be ignored if
   // set_cycle is not 0.
-  event->setTriggerTime(2.2);
+  m_scheduledEvent->setTriggerTime(2.2);
 
   // Activate the event
-  event->activate();
-
-  // Add the event to the event manager
-  event_manager_add_event(event);
+  m_scheduledEvent->activate();
 }
