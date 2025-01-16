98d4d0731177e74738d1b9b38f6986d18e2ac99c
rendayigit <renda.yigit@gmail.com>
2025-01-16 15:40:51 +0300

    document S_overrides.mk

--- S_overrides.mk
+++ S_overrides.mk
@@ -1,4 +1,10 @@
-# Add -g for debugging purposes
+# Add include paths
+FLAGS += -Imodels										# Add simulation models include path
 
-TRICK_CFLAGS += -Imodels -g
-TRICK_CXXFLAGS += -Imodels -g
+# Add compile flags
+FLAGS += -pthread										# Enable multi threading
+FLAGS += -g 											# Needed for debugging simulation
+
+# Add defines, includes and other compile flags to C and CXX flags
+TRICK_CFLAGS += $(FLAGS)
+TRICK_CXXFLAGS += $(FLAGS)
