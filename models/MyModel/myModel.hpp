b70720e85e323e0db26899be4479066d5de9d13b
rendayigit <renda.yigit@gmail.com>
2025-01-24 10:45:07 +0300

    remove unnecessary code from trick-tv-issue

--- models/MyModel/myModel.hpp
+++ models/MyModel/myModel.hpp
@@ -1,23 +1,13 @@
 #include <array>
-#include <iostream>
-#include <string>
 #include <vector>
-
-class Foo {
-public:
-  int a = 5;
-  std::string someString = "hello";
-};
 
 class MyModel {
 public:
-  Foo *foo;
   std::vector<int> bar;
   std::array<int, 2> bar2;
   int bar3[2];
 
   MyModel() {
-    foo = new Foo;
     bar.push_back(1);
     bar.push_back(2);
     bar.push_back(3);
@@ -28,7 +18,5 @@ public:
 
     bar3[0] = 7;
     bar3[1] = 8;
-
-    std::cout << bar[0] << std::endl;
   }
 };
