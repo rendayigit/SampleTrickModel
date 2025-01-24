ee9ea5335e2ed73d95fc10a11be5dd8d84b6cf0f
rendayigit <renda.yigit@gmail.com>
2025-01-24 10:07:54 +0300

    trick-tv-issue

--- models/MyModel/myModel.hpp
+++ models/MyModel/myModel.hpp
@@ -1,4 +1,7 @@
+#include <array>
+#include <iostream>
 #include <string>
+#include <vector>
 
 class Foo {
 public:
@@ -9,6 +12,23 @@ public:
 class MyModel {
 public:
   Foo *foo;
+  std::vector<int> bar;
+  std::array<int, 2> bar2;
+  int bar3[2];
 
-  MyModel() { foo = new Foo; }
+  MyModel() {
+    foo = new Foo;
+    bar.push_back(1);
+    bar.push_back(2);
+    bar.push_back(3);
+    bar.push_back(4);
+
+    bar2[0] = 5;
+    bar2[1] = 6;
+
+    bar3[0] = 7;
+    bar3[1] = 8;
+
+    std::cout << bar[0] << std::endl;
+  }
 };
